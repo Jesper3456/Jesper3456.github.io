0ab6313c04f2c0d86ed0750659e6914e251f5003
Jesper3456 <135592110+Jesper3456@users.noreply.github.com>
2024-03-07 16:26:53 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     document.querySelector('#image-body').style.display = 'block';
-    document.querySelectorAll('#gallery').style.display = 'none';
+    document.querySelector('#gallery').style.display = 'none';
 
 
     document.querySelector('#galleryLink').addEventListener('onclick', () => {
