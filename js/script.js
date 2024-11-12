318b318dcfbbee0963f01d3c05249623f356075b
Jesper3456 <135592110+Jesper3456@users.noreply.github.com>
2024-11-12 18:00:12 +0100

    more js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
-document.getElementById('btnex1').addEventListener('click', () => {
-    let solution = document.getElementById('schwingungsdauer1').innerHTML;
-    if (solution.localeCompare('1,23')) {
-        document.getElementById('schwingungsdauer1').style.borderColor = 'green';
-    } else {
-        document.getElementById('schwingungsdauer1').style.borderColor = 'red'
-    }
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('btnex1').addEventListener('click', () => {
+        let solution = document.getElementById('schwingungsdauer1').value;
+        console.log(solution)
+        if (solution.localeCompare('1,23') === 0) {
+            document.getElementById('schwingungsdauer1').style.borderColor = 'green';
+        } else {
+            document.getElementById('schwingungsdauer1').style.borderColor = 'red'
+        }
+    })
 })
