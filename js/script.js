a69cdc40a907f83f07512ac06e26ace3222905aa
Jesper3456 <135592110+Jesper3456@users.noreply.github.com>
2024-11-13 08:03:25 +0100

    more exercises

--- js/script.js
+++ js/script.js
@@ -1,11 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('btnex1').addEventListener('click', () => {
-        let solution = document.getElementById('schwingungsdauer1').value;
-        console.log(solution)
-        if (solution.localeCompare('1,23') === 0) {
+        let solution1 = document.getElementById('schwingungsdauer1').value;
+        if (solution1.localeCompare('1,23') === 0) {
             document.getElementById('schwingungsdauer1').style.borderColor = 'green';
         } else {
             document.getElementById('schwingungsdauer1').style.borderColor = 'red'
+        }
+    })
+    document.getElementById('btnex2').addEventListener('click', () => {
+        let solution2 = document.getElementById('pendellaenge2').value;
+        console.log(solution2)
+        if (solution2.localeCompare('0,64') === 0) {
+            document.getElementById('pendellaenge2').style.borderColor = 'green';
+        } else {
+            document.getElementById('pendellaenge2').style.borderColor = 'red';
         }
     })
 })
